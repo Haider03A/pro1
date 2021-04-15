5680bb4fd058c6416ca8435becc9615c8728388f
pc <polkmn3612@gmail.com>
2021-04-15 14:28:11 +0300

    click span in js

--- js/min.js
+++ js/min.js
@@ -5,4 +5,10 @@
   } else {
     link.style.display = "block";
   }
+  var span = document.getElementById("span");
+  if (span.style.width === "100%") {
+    span.style.width = "50%";
+  } else {
+    span.style.width = "100%";
+  }
 }
