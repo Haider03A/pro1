9582ce91480cb8287a8b94c37240dc2a487b3333
pc <polkmn3612@gmail.com>
2021-04-16 18:35:55 +0300

    Update min.js

--- js/min.js
+++ js/min.js
@@ -2,7 +2,17 @@ var // Var Header
     link = document.querySelector(".header .contener .link"),
     iconTransform = document.querySelector(".header .contener .link .icon"),
     nav = document.querySelector(".header .contener .link .nav");
-link.onclick = function () {
+link.onclick = function (e) {
+    e.stopPropagation(),
     iconTransform.classList.toggle("iconTransform"),
     nav.classList.toggle("open");
 };
+
+document.addEventListener("click", (e) =>{
+    if (e.iconTransform !== link ) {
+        if (nav.classList.contains("open")) {
+            iconTransform.classList.toggle("iconTransform"),
+             nav.classList.toggle("open");
+        }
+    }
+})
