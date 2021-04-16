01e50873f5d6de66680985ff715d7b558e55fd2b
pc <polkmn3612@gmail.com>
2021-04-16 17:21:58 +0300

    js

--- js/min.js
+++ js/min.js
@@ -1,14 +1,8 @@
- document.querySelector(".link").onclick = function () {
-    var link = document.getElementById("nav");
-  if (link.style.display === "block") {
-    link.style.display = "none";
-  } else {
-    link.style.display = "block";
-  }
-  var span = document.getElementById("span");
-  if (span.style.width === "100%") {
-    span.style.width = "50%";
-  } else {
-    span.style.width = "100%";
-  }
-}
+var // Var Header
+    link = document.querySelector(".header .contener .link"),
+    iconTransform = document.querySelector(".header .contener .link .icon"),
+    nav = document.querySelector(".header .contener .link .nav");
+link.onclick = function () {
+    iconTransform.classList.toggle("iconTransform"),
+    nav.classList.toggle("open");
+};
