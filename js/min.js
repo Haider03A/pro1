8a4bb406e27fd90c4a3297b45196769a8f1f0dce
pc <polkmn3612@gmail.com>
2021-04-27 22:30:23 +0300

    Update min.js

--- js/min.js
+++ js/min.js
@@ -2,16 +2,23 @@ var // Var Header
     link = document.querySelector(".header .contener .link"),
     iconTransform = document.querySelector(".header .contener .link .icon"),
     nav = document.querySelector(".header .contener .link .nav");
-link.onclick = function (e) {
-    e.stopPropagation(),
-    iconTransform.classList.toggle("iconTransform"),
-    nav.classList.toggle("open");
+var hi = prompt("Hi there how are yuo?");
+
+if (hi !== "3612") {
+    location.reload();
 };
-document.addEventListener("click", (e) =>{
-    if (e.iconTransform !== link ) {
+
+link.onclick = function(e) {
+    e.stopPropagation(),
+        iconTransform.classList.toggle("iconTransform"),
+        nav.classList.toggle("open");
+};
+
+document.addEventListener("click", (e) => {
+    if (e.iconTransform !== link) {
         if (nav.classList.contains("open")) {
             iconTransform.classList.toggle("iconTransform"),
-             nav.classList.toggle("open");
+                nav.classList.toggle("open");
         }
     }
 })
