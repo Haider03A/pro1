24c8e88622ecd873262b27ed41f6d53f56d6097d
hayderali3612 <hayderali3612@gmail.com>
2021-05-21 23:33:43 +0300

    d

---
+++ js/min.js
@@ -0,0 +1,25 @@
+var // Var Header
+    link = document.querySelector(".header .contener .link"),
+    iconTransform = document.querySelector(".header .contener .link .icon"),
+    nav = document.querySelector(".header .contener .link .nav");
+/* var hi = prompt("Hi there how are yuo?");
+
+if (hi !== "3612") {
+    nav.classList.toggle("bodyNone");
+    location.reload();
+}; */
+
+link.onclick = function(e) {
+    e.stopPropagation(),
+        iconTransform.classList.toggle("iconTransform"),
+        nav.classList.toggle("open");
+};
+
+document.addEventListener("click", (e) => {
+    if (e.iconTransform !== link) {
+        if (nav.classList.contains("open")) {
+            iconTransform.classList.toggle("iconTransform"),
+                nav.classList.toggle("open");
+        }
+    }
+})
